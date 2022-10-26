922381b05e9a6b7490721d8b0bd0a9e653d7a0e5
Code Monad <codemonad@cryptape.com>
2022-10-26 22:54:01 +0800

    update code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use sparse_merkle_tree::branch::BranchKey;
 use {sparse_merkle_tree::*};
 use sparse_merkle_tree::merge::hash_base_node;
+use sparse_merkle_tree::tree::SparseMerkleTree as OldTree;
+use sparse_merkle_tree::trie_tree::SparseMerkleTree as TrieMerkleTree;
 #[allow(clippy::upper_case_acronyms)]
-type SMT = SparseMerkleTree<blake2b::Blake2bHasher, H256, default_store::DefaultStore<H256>>;
+type SMT = OldTree<blake2b::Blake2bHasher, H256, default_store::DefaultStore<H256>>;
 type SMT_NEW = TrieMerkleTree<blake2b::Blake2bHasher, H256, default_store::DefaultStore<H256>>;
 use sparse_merkle_tree::merge::MergeValue::{MergeWithZero, ShortCut};
-use sparse_merkle_tree::merge::merge_with_zero;
+//use sparse_merkle_tree::merge::merge_with_zero;
 use sparse_merkle_tree::merge::merge;
 use sparse_merkle_tree::traits::StoreReadOps;
 
@@ -23,12 +25,26 @@ fn main() {
     tree.update(key.into(), value);
     trie_tree.update(key.into(), value);
 
+    let root = trie_tree.root().clone();
 
-    key = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
+    key = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 
     tree.update(key.into(), value);
     trie_tree.update(key.into(), value);
-    println!("root: {:?}, new_root: {:?}", tree.root(), trie_tree.root())
+
+    let branch_key =  BranchKey::new(255, key.into());
+
+    if let Some(branch) = tree.store().get_branch(&branch_key).unwrap() {
+        println!("old branch: {:?}", branch);
+    }
+
+    if let Some(branch) = trie_tree.store().get_branch(&branch_key).unwrap() {
+        println!("new branch: {:?}", branch);
+    }
+
+    println!("root: {:?}, new_root: {:?}", tree.root(), trie_tree.root());
+
+    assert_eq!(tree.root(), trie_tree.root());
 
 }
